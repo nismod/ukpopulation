91433259fe93ef502e8d7cfe737032995a223499
Luke Archer <l.archer@leeds.ac.uk>
2019-11-26 16:22:26 +0000

    Fixed issue with SNPP data when running static microsimulation
    When running the static microsimulation (year by year), each year uses a data source to constrain the microsynthesis. When moving from 2017 to 2018 (projection changed from SNPP (2016 based) to NPP (2018 based)), the split_range() function was returning a list with an empty element, instead of a None type object. This was causing an error in SNPPData.filter() where the normal logic for handling a None type object was not being executed due to the list being real but empty. Now the None type list is converted to None before being passed to filter().

--- tests/setup_test_data.py
+++ tests/setup_test_data.py
@@ -11,38 +11,38 @@ real_data_dir = "~/.ukpopulation/cache/"
 test_data_dir = "./tests/raw_data/"
 
 def setup_snpp_data():
-  """
-  SNPP test data is 3 LADs per country for years 2016-2029 (Including Wales which was previously 2014!)
-  """
-  raw_files = ["NM_2006_1_daab3ca05fa48b4ce204ee108a561c8b.tsv", # England
-              "snpp_w.csv","snpp_s.csv","snpp_ni.csv"]
+    """
+    SNPP test data is 3 LADs per country for years 2016-2029 (Including Wales which was previously 2014!)
+    """
+    raw_files = ["NM_2006_1_daab3ca05fa48b4ce204ee108a561c8b.tsv", # England
+                 "snpp_w.csv", "snpp_s.csv", "snpp_ni.csv"]
 
-  for file in raw_files:
-    sep = "\t" if file[-4:] == ".tsv" else ","
-    df = pd.read_csv(real_data_dir + file, sep=sep)
+    for file in raw_files:
+        sep = "\t" if file[-4:] == ".tsv" else ","
+        df = pd.read_csv(real_data_dir + file, sep=sep)
 
-    geogs = df.GEOGRAPHY_CODE.unique()[:3]
-    df = df[(df.GEOGRAPHY_CODE.isin(geogs)) & (df.PROJECTED_YEAR_NAME <= 2029)]
+        geogs = df.GEOGRAPHY_CODE.unique()[:3]
+        df = df[(df.GEOGRAPHY_CODE.isin(geogs)) & (df.PROJECTED_YEAR_NAME <= 2029)]
 
-    df.to_csv(test_data_dir + file, sep=sep, index=False)
+        df.to_csv(test_data_dir + file, sep=sep, index=False)
 
-  # NB the file NM_2006_1_d76d72a49d14b5e740ea04a14b741101.tsv also needs to be in the test data folder,
-  # containing column headings only. (This will prevent the data being re-downloaded)
+    # NB the file NM_2006_1_d76d72a49d14b5e740ea04a14b741101.tsv also needs to be in the test data folder,
+    # containing column headings only. (This will prevent the data being re-downloaded)
 
 def setup_npp_data():
-  """
-  NPP test data is 3 variants, all ages, for years 2018-2035
-  """
-  raw_files = ["NM_2009_1_a312e4ffeb62374b4277cb8711d68b78.tsv", # ppp
-              "npp_hhh.csv","npp_lll.csv"]
-  
-  for file in raw_files:
-    sep = "\t" if file[-4:] == ".tsv" else ","
-    df = pd.read_csv(real_data_dir + file, sep=sep)
-    df = df[(df.PROJECTED_YEAR_NAME < 2036)]
-    df.to_csv(test_data_dir + file, sep=sep, index=False)
+    """
+    NPP test data is 3 variants, all ages, for years 2018-2035
+    """
+    raw_files = ["NM_2009_1_a312e4ffeb62374b4277cb8711d68b78.tsv", # ppp
+                 "npp_hhh.csv", "npp_lll.csv"]
+
+    for file in raw_files:
+        sep = "\t" if file[-4:] == ".tsv" else ","
+        df = pd.read_csv(real_data_dir + file, sep=sep)
+        df = df[(df.PROJECTED_YEAR_NAME < 2036)]
+        df.to_csv(test_data_dir + file, sep=sep, index=False)
 
 
 if __name__ == '__main__':
-  # setup_snpp_data()
-  setup_npp_data()
+    # setup_snpp_data()
+    setup_npp_data()
