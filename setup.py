c25425cc19e9afc7f403c19fe46ff381fc3ac1b3
Andrew Smith <andrew@friarswood.net>
2020-11-03 15:12:18 +0000

    1.2.2 release

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ def read_reqs():
 
 setuptools.setup(
   name='ukpopulation',
-  version="1.2.1",
+  version="1.2.2",
   description='Download, cache, collate, filter, manipulate and extrapolate UK population and household estimates/projections',
   long_description=readme(),
   long_description_content_type="text/markdown",
